f8b240b24fda6be93d1e7cd2526e9fa45dcd4f5e
Ian Blake <ian@ianbl8.com>
2024-02-15 21:44:41 +0000

    Log in page with routes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,7 @@
 <div class="container h-full mx-auto">
 	<!-- Page name and route -->
 	<div class="pt-8 pb-4">
-		<h1 class="text-4xl font-bold">Page</h1>
+		<h1 class="text-4xl font-bold">Log in page</h1>
 		<h2 class="text-2xl font-semibold">
 			<span class="font-mono text-stone-700 dark:text-stone-300 bg-stone-300 dark:bg-stone-700">
 				/
@@ -14,25 +14,64 @@
 
 		<!-- Section -->
 		<div class="py-3">
-			<h3 class="h3 py-1">Section</h3>
+			<h3 class="h3 py-1">Log in by email</h3>
 			<p class="py-1">
 				<span class="font-mono text-stone-700 dark:text-stone-300 bg-stone-300 dark:bg-stone-700">
 					Roles: A T S R
 				</span>
 			</p>
 			<p class="py-1">
-				Description
+				Log in links to:
 				<a
-					href="/"
+					href="/dashboard"
 					class="font-mono text-tertiary-800 dark:text-tertiary-200 bg-tertiary-200 dark:bg-tertiary-800"
 				>
-					Link
+					/dashboard
 				</a>
 			</p>
 			<!-- Sub section -->
 			<hr />
+			<p class="py-1">
+				Sign up links to:
+				<a
+					href="/user/new"
+					class="font-mono text-tertiary-800 dark:text-tertiary-200 bg-tertiary-200 dark:bg-tertiary-800"
+				>
+					/user/new
+				</a>
+			</p>
 		</div>
 
-	</div>
+		<!-- Section -->
+		<div class="py-3">
+			<h3 class="h3 py-1">Log in with GitHub</h3>
+			<p class="py-1">
+				<span class="font-mono text-stone-700 dark:text-stone-300 bg-stone-300 dark:bg-stone-700">
+					Roles: A T S R
+				</span>
+			</p>
+			<p class="py-1">
+				Log in links to:
+				<a
+					href="/dashboard"
+					class="font-mono text-tertiary-800 dark:text-tertiary-200 bg-tertiary-200 dark:bg-tertiary-800"
+				>
+					/dashboard
+				</a>
+			</p>
+			<!-- Sub section -->
+			<hr />
+			<p class="py-1">
+				First log in links to:
+				<a
+					href="/user/new"
+					class="font-mono text-tertiary-800 dark:text-tertiary-200 bg-tertiary-200 dark:bg-tertiary-800"
+				>
+					/user/new
+				</a>
+			</p>
+		</div>
+
+  </div>
 
 </div>
