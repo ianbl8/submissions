c44c7abffe8822d8e653a78aa27fa50c004fcad4
Ian Blake <ian@ianbl8.com>
2024-02-15 22:30:51 +0000

    Module pages with routes

---
+++ src/routes/module/[mid]/+page.svelte
@@ -0,0 +1,100 @@
+<div class="container h-full mx-auto">
+	<!-- Page name and route -->
+	<div class="pt-8 pb-4">
+		<h1 class="text-4xl font-bold">Module</h1>
+		<h2 class="text-2xl font-semibold">
+			<span class="font-mono text-stone-700 dark:text-stone-300 bg-stone-300 dark:bg-stone-700">
+				/module/[mid]
+			</span>
+		</h2>
+	</div>
+
+	<!-- Page content -->
+	<div class="flex flex-col">
+
+		<!-- Section -->
+		<div class="py-3">
+			<h3 class="h3 py-1">Module details</h3>
+			<p class="py-1">
+				<span class="font-mono text-stone-700 dark:text-stone-300 bg-stone-300 dark:bg-stone-700">
+					Roles: A T S R
+				</span>
+			</p>
+			<p class="py-1">Module details</p>
+			<!-- Sub section -->
+			<hr />
+			<p class="py-1">
+				<span class="font-mono text-stone-700 dark:text-stone-300 bg-stone-300 dark:bg-stone-700">
+					Roles: S
+				</span>
+			</p>
+			<p class="py-1">
+				Released module grade (when complete)
+			</p>
+				<!-- Sub section -->
+			<hr />
+			<p class="py-1">
+				<span class="font-mono text-stone-700 dark:text-stone-300 bg-stone-300 dark:bg-stone-700">
+					Roles: A T
+				</span>
+			</p>
+			<p class="py-1">
+				Link to edit module details:
+				<a
+					href="/module/mid/edit"
+					class="font-mono text-tertiary-800 dark:text-tertiary-200 bg-tertiary-200 dark:bg-tertiary-800"
+				>
+					/module/[mid]/edit
+				</a>
+			</p>
+		</div>
+
+		<!-- Section -->
+		<div class="py-3">
+			<h3 class="h3 py-1">Assignments</h3>
+			<p class="py-1">
+				<span class="font-mono text-stone-700 dark:text-stone-300 bg-stone-300 dark:bg-stone-700">
+					Roles: A T S R
+				</span>
+			</p>
+			<p class="py-1">List of assignments</p>
+			<p class="py-1">
+				Link to each assignment:
+				<a
+					href="/assignment/aid"
+					class="font-mono text-tertiary-800 dark:text-tertiary-200 bg-tertiary-200 dark:bg-tertiary-800"
+				>
+					/assignment/[aid]
+				</a>
+			</p>
+			<!-- Sub section -->
+			<hr />
+			<p class="py-1">
+				<span class="font-mono text-stone-700 dark:text-stone-300 bg-stone-300 dark:bg-stone-700">
+					Roles: S
+				</span>
+			</p>
+			<p class="py-1">
+				Released grade for each assignment (when complete)
+			</p>
+			<!-- Sub section -->
+			<hr />
+			<p class="py-1">
+				<span class="font-mono text-stone-700 dark:text-stone-300 bg-stone-300 dark:bg-stone-700">
+					Roles: A T
+				</span>
+			</p>
+			<p class="py-1">
+				Link to create new assignment:
+				<a
+					href="/assignment/new"
+					class="font-mono text-tertiary-800 dark:text-tertiary-200 bg-tertiary-200 dark:bg-tertiary-800"
+				>
+					/assignment/new
+				</a>
+			</p>
+		</div>
+
+	</div>
+
+</div>
