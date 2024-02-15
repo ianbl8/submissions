bb748a2107f1f95f524f746c14e97ee42fe1b314
Ian Blake <ian@ianbl8.com>
2024-02-15 21:59:08 +0000

    User pages with routes

---
+++ src/routes/user/[uid]/edit/+page.svelte
@@ -0,0 +1,38 @@
+<div class="container h-full mx-auto">
+	<!-- Page name and route -->
+	<div class="pt-8 pb-4">
+		<h1 class="text-4xl font-bold">Edit User</h1>
+		<h2 class="text-2xl font-semibold">
+			<span class="font-mono text-stone-700 dark:text-stone-300 bg-stone-300 dark:bg-stone-700">
+				/user/[uid]/edit
+			</span>
+		</h2>
+	</div>
+
+	<!-- Page content -->
+	<div class="flex flex-col">
+
+		<!-- Section -->
+		<div class="py-3">
+			<h3 class="h3 py-1">Profile</h3>
+			<p class="py-1">
+				<span class="font-mono text-stone-700 dark:text-stone-300 bg-stone-300 dark:bg-stone-700">
+					Roles: A T S R
+				</span>
+			</p>
+			<p class="py-1">Form to edit user details</p>
+			<p class="py-1">
+				Return to
+				<a
+					href="/user/uid"
+					class="font-mono text-tertiary-800 dark:text-tertiary-200 bg-tertiary-200 dark:bg-tertiary-800"
+				>
+					/user/[uid]
+				</a>
+				when done
+			</p>
+		</div>
+
+	</div>
+	
+</div>
---
+++ src/routes/user/admin/+page.svelte
@@ -0,0 +1,47 @@
+<div class="container h-full mx-auto">
+	<!-- Page name and route -->
+	<div class="pt-8 pb-4">
+		<h1 class="text-4xl font-bold">User Admin</h1>
+		<h2 class="text-2xl font-semibold">
+			<span class="font-mono text-stone-700 dark:text-stone-300 bg-stone-300 dark:bg-stone-700">
+				/user/admin
+			</span>
+		</h2>
+	</div>
+
+	<!-- Page content -->
+	<div class="flex flex-col">
+
+		<!-- Section -->
+		<div class="py-3">
+			<h3 class="h3 py-1">Users</h3>
+			<p class="py-1">
+				<span class="font-mono text-stone-700 dark:text-stone-300 bg-stone-300 dark:bg-stone-700">
+					Roles: A
+				</span>
+			</p>
+			<p class="py-1">List of users</p>
+			<p class="py-1">For each user:</p>
+			<p class="py-1">
+				– Link to user page:
+				<a
+					href="/user/uid"
+					class="font-mono text-tertiary-800 dark:text-tertiary-200 bg-tertiary-200 dark:bg-tertiary-800"
+				>
+					/user/[uid]
+				</a>
+			</p>
+			<p class="py-1">
+				– Link to edit user page:
+				<a
+					href="/user/uid/edit"
+					class="font-mono text-tertiary-800 dark:text-tertiary-200 bg-tertiary-200 dark:bg-tertiary-800"
+				>
+					/user/[uid]/edit
+				</a>
+			</p>
+		</div>
+
+	</div>
+
+</div>
