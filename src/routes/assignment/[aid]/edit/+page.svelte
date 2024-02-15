b14e7327746946c8859e4594ab23c97939762ef3
Ian Blake <ian@ianbl8.com>
2024-02-15 22:50:54 +0000

    Assignment pages with routes

---
+++ src/routes/assignment/[aid]/edit/+page.svelte
@@ -0,0 +1,37 @@
+<div class="container h-full mx-auto">
+	<!-- Page name and route -->
+	<div class="pt-8 pb-4">
+		<h1 class="text-4xl font-bold">Edit Assignment</h1>
+		<h2 class="text-2xl font-semibold">
+			<span class="font-mono text-stone-700 dark:text-stone-300 bg-stone-300 dark:bg-stone-700">
+				/assignment/[aid]/edit
+			</span>
+		</h2>
+	</div>
+
+	<!-- Page content -->
+	<div class="flex flex-col">
+
+		<!-- Section -->
+		<div class="py-3">
+			<h3 class="h3 py-1">Assignment details</h3>
+			<p class="py-1">
+				<span class="font-mono text-stone-700 dark:text-stone-300 bg-stone-300 dark:bg-stone-700">
+					Roles: A T
+				</span>
+			</p>
+			<p class="py-1">Populated form for assignment details</p>
+			<p class="py-1">
+				Link to assignment when complete:
+				<a
+					href="/assignment/aid"
+					class="font-mono text-tertiary-800 dark:text-tertiary-200 bg-tertiary-200 dark:bg-tertiary-800"
+				>
+					/assignment/[aid]
+				</a>
+			</p>
+		</div>
+
+	</div>
+
+</div>
---
+++ src/routes/assignment/[aid]/grade/[sid]/+page.svelte
@@ -0,0 +1,45 @@
+<div class="container h-full mx-auto">
+	<!-- Page name and route -->
+	<div class="pt-8 pb-4">
+		<h1 class="text-4xl font-bold">Grade Submission</h1>
+		<h2 class="text-2xl font-semibold">
+			<span class="font-mono text-stone-700 dark:text-stone-300 bg-stone-300 dark:bg-stone-700">
+				/assignment/[aid]/grade/[sid]
+			</span>
+		</h2>
+	</div>
+
+	<!-- Page content -->
+	<div class="flex flex-col">
+
+		<!-- Section -->
+		<div class="py-3">
+			<h3 class="h3 py-1">Submission details</h3>
+			<p class="py-1">
+				<span class="font-mono text-stone-700 dark:text-stone-300 bg-stone-300 dark:bg-stone-700">
+					Roles: A T R
+				</span>
+			</p>
+			<p class="py-1">Submission details and links</p>
+			<!-- Sub section -->
+			<hr />
+			<p class="py-1">
+				<span class="font-mono text-stone-700 dark:text-stone-300 bg-stone-300 dark:bg-stone-700">
+					Roles: A T
+				</span>
+			</p>
+			<p class="py-1">Grading form</p>
+			<p class="py-1">
+				Link to grading page when complete:
+				<a
+					href="/assignment/aid/grade"
+					class="font-mono text-tertiary-800 dark:text-tertiary-200 bg-tertiary-200 dark:bg-tertiary-800"
+				>
+					/assignment/[aid]/grade
+				</a>
+			</p>
+		</div>
+
+	</div>
+
+</div>
