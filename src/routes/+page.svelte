f13dd6a0d0eff4c3096f30d8eb52e5b70a73ac00
Ian Blake <ian@ianbl8.com>
2024-02-19 20:19:36 +0000

    Tidied log in page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -16,6 +16,7 @@
 		</h2>
 	</div>
 
+	<!-- Page content -->
 	<!-- Supabase Auth -->
 	<div class="flex flex-col">
 		<div class="py-3 ">
@@ -29,66 +30,4 @@
 		</div>
 	</div>
 
-	<!-- Page content -->
-	<div class="flex flex-col">
-		<!-- Section -->
-		<div class="py-3">
-			<h3 class="h3 py-1">Log in by email</h3>
-			<p class="py-1">
-				<span class="font-mono text-stone-700 dark:text-stone-300 bg-stone-300 dark:bg-stone-700">
-					Roles: A T S R
-				</span>
-			</p>
-			<p class="py-1">
-				Log in links to:
-				<a
-					href="/dashboard"
-					class="font-mono text-tertiary-800 dark:text-tertiary-200 bg-tertiary-200 dark:bg-tertiary-800"
-				>
-					/dashboard
-				</a>
-			</p>
-			<!-- Sub section -->
-			<hr />
-			<p class="py-1">
-				Sign up links to:
-				<a
-					href="/user/new"
-					class="font-mono text-tertiary-800 dark:text-tertiary-200 bg-tertiary-200 dark:bg-tertiary-800"
-				>
-					/user/new
-				</a>
-			</p>
-		</div>
-
-		<!-- Section -->
-		<div class="py-3">
-			<h3 class="h3 py-1">Log in with GitHub</h3>
-			<p class="py-1">
-				<span class="font-mono text-stone-700 dark:text-stone-300 bg-stone-300 dark:bg-stone-700">
-					Roles: A T S R
-				</span>
-			</p>
-			<p class="py-1">
-				Log in links to:
-				<a
-					href="/dashboard"
-					class="font-mono text-tertiary-800 dark:text-tertiary-200 bg-tertiary-200 dark:bg-tertiary-800"
-				>
-					/dashboard
-				</a>
-			</p>
-			<!-- Sub section -->
-			<hr />
-			<p class="py-1">
-				First log in links to:
-				<a
-					href="/user/new"
-					class="font-mono text-tertiary-800 dark:text-tertiary-200 bg-tertiary-200 dark:bg-tertiary-800"
-				>
-					/user/new
-				</a>
-			</p>
-		</div>
-	</div>
 </div>
