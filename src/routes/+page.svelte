a80e14bffab2c6f53266715b4fbff1e5e8ac4d39
Ian Blake <ian@ianbl8.com>
2024-02-27 21:32:57 +0000

    Update login page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,20 +6,18 @@
 </script>
 
 <div class="container h-full mx-auto">
-	<!-- Page name and route -->
+	<!-- Page name -->
 	<div class="pt-8 pb-4">
-		<h1 class="text-4xl font-bold">Log in page</h1>
-		<h2 class="text-2xl font-semibold">
-			<span class="font-mono text-stone-700 dark:text-stone-300 bg-stone-300 dark:bg-stone-700">
-				/
-			</span>
-		</h2>
+		<h1 class="text-4xl font-semibold">
+			Welcome to <span class="font-bold text-primary-600 dark:text-primary-400">Submissions</span>
+		</h1>
+		<h3 class="h3 py-1 mt-4">Please log in or sign up</h3>
 	</div>
 
 	<!-- Page content -->
 	<!-- Supabase Auth -->
 	<div class="flex flex-col">
-		<div class="py-3 ">
+		<div class="py-3">
 			<Auth
 				supabaseClient={data.supabase}
 				redirectTo={`${data.url}/auth/callback`}
@@ -29,5 +27,4 @@
 			/>
 		</div>
 	</div>
-
 </div>
