ff7cf0479e7f1e50141af5e10bbe1cd725c20e31
Ian Blake <ian@ianbl8.com>
2024-03-07 18:28:13 +0000

    Implement full-featured datatable for user admin

---
+++ src/lib/components/datatables/RowsPerPage.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import type { DataHandler } from '@vincjo/datatables';
+	export let handler: DataHandler;
+	const rowsPerPage = handler.getRowsPerPage();
+	const options = [10, 20, 30, 40, 50];
+</script>
+
+<aside class="flex place-items-center">
+	Show
+	<select class="select ml-2" bind:value={$rowsPerPage}>
+		{#each options as option}
+			<option value={option}>
+				{option}
+			</option>
+		{/each}
+	</select>
+  &nbsp;users
+</aside>
--- src/routes/user/admin/+page.svelte
+++ src/routes/user/admin/+page.svelte
@@ -2,7 +2,14 @@
 	import { DataHandler } from '@vincjo/datatables';
 	import type { PageData } from './$types';
 	import PageTitle from '$lib/components/PageTitle.svelte';
-
+	import Search from '$lib/components/datatables/Search.svelte';
+	import ThFilter from '$lib/components/datatables/ThFilter.svelte';
+	import ThSort from '$lib/components/datatables/ThSort.svelte';
+	import RowCount from '$lib/components/datatables/RowCount.svelte';
+	import RowsPerPage from '$lib/components/datatables/RowsPerPage.svelte';
+	import Pagination from '$lib/components/datatables/Pagination.svelte';
+	import Page from '../../+page.svelte';
+        
 	export let data: PageData;
 
 	let { session, supabase, users, loggedInUser } = data;
@@ -27,44 +34,29 @@
 		<!-- Section -->
 		<div class="py-3">
 			<h3 class="h3 py-1">Users</h3>
-			<!--
-			{#if users}
-				{#each users as user}
-					<p class="py-1 font-semibold">Number {user.number}</p>
-					<p class="py-1">
-						– Details:
-						<a
-							href="/user/{user.number}"
-							class="font-mono text-tertiary-800 dark:text-tertiary-200 bg-tertiary-200 dark:bg-tertiary-800"
-						>
-							/user/{user.number}
-						</a>
-					</p>
-					<p class="py-1">
-						– Edit:
-						<a
-							href="/user/{user.number}/edit"
-							class="font-mono text-tertiary-800 dark:text-tertiary-200 bg-tertiary-200 dark:bg-tertiary-800"
-						>
-							/user/{user.number}/edit
-						</a>
-					</p>
-						{/each}
-			{:else}
-				<p class="py-1">No users</p>
-			{/if}
-			-->
+			<header class="flex justify-between gap-4">
+				<Search {handler} />
+				<RowsPerPage {handler} />
+			</header>
 			<table class="table table-hover table-compact table-auto w-full">
 				<thead>
 					<tr>
-						<td>Forename</td>
-						<td>Surname</td>
-						<td>Email</td>
-						<td>Role</td>
-						<td>Student ID</td>
-						<td>Status</td>
-						<td>User page</td>
-						<td>Edit user</td>
+						<ThSort {handler} orderBy="forename">Forename</ThSort>
+						<ThSort {handler} orderBy="surname">Surname</ThSort>
+						<th>Email</th>
+						<th>Role</th>
+						<ThSort {handler} orderBy="student_id">Student ID</ThSort>
+						<th>Status</th>
+						<th colspan="2">User details</th>
+					</tr>
+					<tr>
+						<ThFilter {handler} filterBy="forename" />
+						<ThFilter {handler} filterBy="surname" />
+						<ThFilter {handler} filterBy="email" />
+						<ThFilter {handler} filterBy="role" />
+						<ThFilter {handler} filterBy="student_id" />
+						<ThFilter {handler} filterBy="status" />
+						<th colspan="2"></th>
 					</tr>
 				</thead>
 				<tbody>
@@ -76,12 +68,16 @@
 							<td>{row.role}</td>
 							<td>{#if row.student_id}{row.student_id}{:else}—{/if}</td>
 							<td>{row.status}</td>
-							<td><a href="../user/{row.number}">User page</a></td>
-							<td><a href="../user/{row.number}/edit">Edit user</a></td>
+							<td><a class="btn btn-sm variant-ghost-tertiary" href="../user/{row.number}">View</a></td>
+							<td><a class="btn btn-sm variant-ghost-secondary" href="../user/{row.number}/edit">Edit</a></td>
 						</tr>
 					{/each}
 				</tbody>
 			</table>
+			<footer class="flex justify-between">
+				<RowCount {handler} />
+				<Pagination {handler} />
+			</footer>
 		</div>
 
 	</div>
