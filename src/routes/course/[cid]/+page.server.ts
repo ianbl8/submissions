c9a4769dd2db571073be5fd591fac153aaad1cca
Ian Blake <ian@ianbl8.com>
2024-03-14 22:33:18 +0000

    Course page

---
+++ src/routes/course/[cid]/+page.server.ts
@@ -0,0 +1,31 @@
+import { redirect } from "@sveltejs/kit";
+
+export const load = async ({ params, locals: { supabase, getSession } }) => {
+
+  // if not logged in, redirect to landing page
+  const session = await getSession();
+  if (!session) {
+    throw redirect(303, "/");
+  }
+
+  // get course from database
+  const { data: course } = await supabase
+    .from('courses')
+    .select('id, number, name, code, start_date, end_date')
+    .eq('number', params.cid)
+    .single()
+
+  // redirect if not a valid course  
+  if (!course) {
+    throw redirect(303, "/");
+  }
+    
+  // get logged in user from database
+  const { data: loggedInUser } = await supabase
+    .from('users')
+    .select('id, number, forename, surname, email, role, status, student_id')
+    .eq('id', session.user.id)
+    .single()
+
+  return { session, course, loggedInUser }
+}
